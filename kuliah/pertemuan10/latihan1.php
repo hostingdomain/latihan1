61466a3d28335902f9984a4d6fed579d87627f06
H Domain <hostingdomain2019@gmail.com>
2021-09-16 07:49:45 +0700

    rename 2 file database

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/izoel.jpg" width="60"></td>
+      <td>123456789</td>
+      <td>Zulkarnaen</td>
+      <td>izoel@gmail.com</td>
+      <td>Sistem Infomatika</td>
+      <td><a href="">Ubah</a> | <a href="">Hapus</a></td>
+    </tr>
+  </table>
+</body>
+
+</html>
